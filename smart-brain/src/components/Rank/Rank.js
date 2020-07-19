2adeb6e51c3bae4b2808a20b3031764f5d34f039
webguy83 <curtisyacboski@gmail.com>
2020-07-18 22:57:37 -0700

    added ranking

--- smart-brain/src/components/Rank/Rank.js
+++ smart-brain/src/components/Rank/Rank.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState("");
+
+  useEffect(() => {
+    fetch(`https://sjx5jfrccl.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+      .then(res => res.json())
+      .then(data => {
+        setEmoji(data.input);
+      })
+      .catch(console.log)
+  }, [entries]);
+
   return (
     <div>
       <div className='white f3'>
         {`${name}, your current entry count is...`}
       </div>
       <div className='white f1'>
-        {entries}
+        {entries} {emoji}
       </div>
     </div>
   );
